0dd5a141cae93b867227167e3667a1175dcfe006
zefelix <jophev3@hotmail.com>
2020-03-14 17:50:02 -0300

    update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,7 +5,7 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ message: 'User already exists!' });
+      return res.status(400).json({ message: 'Email already exists!' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
@@ -13,8 +13,30 @@ class UserController {
     return res.json({ id, name, email, provider });
   }
 
-  update(req, res) {
-    return res.json({ message: 'ok', id: req.userId, idParams: req.params.id });
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      return res.status(400).json({ message: `User not exists! id: ${id}` });
+    }
+
+    if (email != user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ message: 'User already exists!' });
+      }
+    }
+
+    if (oldPassword && !await (user.checkPassword(oldPassword))) {
+      return res.status(400).json({ message: 'Password does not match!' });
+    }
+    
+    const { name, provider } = await User.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
