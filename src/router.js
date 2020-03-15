4f7a6611853fd236baef7e8560837869ad4847b8
zefelix <jophev3@hotmail.com>
2020-03-15 13:54:51 -0300

    files upload

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const uploads = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,5 +16,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users/:id', UserController.update);
+routes.post('/files', uploads.single('file'), FileController.store);
 
 export default routes;
