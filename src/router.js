c95d18c466d2e5860fc26976458c0bc10f9a038f
zefelix <jophev3@hotmail.com>
2020-03-15 16:37:42 -0300

    get providers and access files statics

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -17,5 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users/:id', UserController.update);
 routes.post('/files', uploads.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
