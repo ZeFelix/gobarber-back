8fc896d49772f29991d1364c07cbab5df7883dd4
zefelix <jophev3@hotmail.com>
2020-03-28 17:58:01 -0300

    config email template

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -80,7 +80,7 @@ class AppoitmentController {
     const user = await User.findByPk(req.userId);
     const formattedDate = format(
       hourStart,
-      " 'dia' dd 'de' MMMM', às H:mm'h",
+      " 'dia' dd 'de' MMMM', às' H:mm'h'",
       { locale: pt },
     );
 
@@ -100,6 +100,11 @@ class AppoitmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -118,7 +123,16 @@ class AppoitmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'VocÊ tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          appointment.date,
+          " 'dia' dd 'de' MMMM', às' H:mm'h'",
+          { locale: pt },
+        ),
+      },
     });
 
     return res.json(appointment);
