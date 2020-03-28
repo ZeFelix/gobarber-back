f2be6e6441a16adcc945ca9d66cf4baf011fa525
zefelix <jophev3@hotmail.com>
2020-03-28 18:47:45 -0300

    create jobs with redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notifications';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 
 class AppoitmentController {
@@ -120,20 +121,7 @@ class AppoitmentController {
 
     await appointment.update({ canceled_at: new Date() });
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(
-          appointment.date,
-          " 'dia' dd 'de' MMMM', às' H:mm'h'",
-          { locale: pt },
-        ),
-      },
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
